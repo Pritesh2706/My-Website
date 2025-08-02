b46088c271125012b50818ac69b828bb092d0ad1
Pritesh2706 <priteshbhere9356@gmail.com>
2025-08-02 22:08:39 +0530

    Update settings.py

--- Demo-portfolio-django-project-main/portfolio/portfolio/settings.py
+++ Demo-portfolio-django-project-main/portfolio/portfolio/settings.py
@@ -77,11 +77,9 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=str(BASE_DIR / 'db.sqlite3'))
 }
 
 
