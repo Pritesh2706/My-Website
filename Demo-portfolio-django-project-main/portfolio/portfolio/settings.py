ea748aa41da5e2a4a5d07dcd39ffa9f2b619b848
Pritesh2706 <priteshbhere9356@gmail.com>
2025-08-02 21:32:01 +0530

    Update settings.py

--- Demo-portfolio-django-project-main/portfolio/portfolio/settings.py
+++ Demo-portfolio-django-project-main/portfolio/portfolio/settings.py
@@ -127,6 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://my-website-production-548e.up.railway.app',
+]
+
 STATICFILES_DIRS=[
   os.path.join(BASE_DIR, 'static')  
 ]
