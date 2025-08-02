e7488ae51c04f9e580d6d9b26f332d50b8ef1633
Pritesh2706 <priteshbhere9356@gmail.com>
2025-08-02 22:56:18 +0530

    Update urls.py

--- Demo-portfolio-django-project-main/portfolio/Base/urls.py
+++ Demo-portfolio-django-project-main/portfolio/Base/urls.py
@@ -1,6 +1,7 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path 
+from . import views
 
-urlpatterns = [
-    path('', views.contact),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns=[
+    path('',views.contact),
+    # path('contact/',views.contact,name="savecontact")
+]
