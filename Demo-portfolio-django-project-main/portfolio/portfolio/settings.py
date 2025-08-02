eda00cc9ab8b0d3e47b892d430d1441cf905a9f3
Pritesh2706 <priteshbhere9356@gmail.com>
2025-08-02 20:43:03 +0530

    Update settings.py

--- Demo-portfolio-django-project-main/portfolio/portfolio/settings.py
+++ Demo-portfolio-django-project-main/portfolio/portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'nv)=_h7=(#+dn7hk2#n17v406lzs#2-)l8kepd#y(u2g)g5_%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["my-website-production-548e.up.railway.app"]
 
@@ -123,8 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICSTORAGE= 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS=[
   os.path.join(BASE_DIR, 'static')  
 ]
